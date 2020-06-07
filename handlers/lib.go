a2dfe60b8b38102d9beefc40e5a863ddff3c3272
NEKETSKY <nikitaverbovikov@gmail.com>
2020-06-07 15:56:45 +0300

    almost done2

--- handlers/lib.go
+++ handlers/lib.go
@@ -7,7 +7,6 @@ import (
 )
 
 func ReturnError(w http.ResponseWriter, data interface{}, code int) {
-	//попробовать какой выводу будет если не передавать дату, если всё плохо - сделать блок условия
 	message, err := json.Marshal(data)
 	if err != nil {
 		log.Fatal(err)
